4ff4c093c5a22a14d242424b0712073517682219
Robert R. Meyer <Blue.Dog.Archolite@gmail.com>
2013-05-16 11:18:48 -0500

    Ensure response is returned from pay calls

--- lib/venmo/pay.rb
+++ lib/venmo/pay.rb
@@ -1,21 +1,21 @@
 module Venmo
   module Pay
     def pay_by_email(email, amount, note)
+      # -d email= \
       @send_hash= common(amount,note, { :email => email})
       core_pay
-      # -d email= \
     end
 
     def pay_by_user_id(user_id, amount, note)
+      # -d user_id= \
       @send_hash= common(amount,note, {:user_id => user_id})
       core_pay
-      # -d user_id= \
     end
 
     def pay_by_phone_number(phone_number, amount, note)
+      # -d phone= \
       @send_hash = common(amount,note, {:phone_number=> phone_number})
       core_pay
-      # -d phone= \
     end
 
     protected
@@ -32,7 +32,7 @@ module Venmo
       base_url = "curl https://api.venmo.com/payments/?"
       h = @send_hash.collect{|k,v| "#{k.to_s}=#{v.respond_to?(:gsub) ? CGI::escape(v) : v}"}.join('&')
       to_send = base_url + h
-      resp = `#{to_send}`
+      `#{to_send}`
     end
   end
 end
